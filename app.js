ee51956dc7ff3a343700c1a0ac560a817822a02b
Dario Villanueva <dario.villanueva@gmail.com>
2014-02-16 18:45:52 +0000

    Updates

--- app.js
+++ app.js
@@ -271,6 +271,7 @@ module.exports = function() {
           this.term.echo('You crashed at ' + this.bird.y );
         }
         this.term.echo('********************\n');
+        this.term.echo('\nYour Score was '+ this.score);
         this.term.echo('\ntype restart to try again.\n');
         return
       }
